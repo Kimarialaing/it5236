c0ce49a6f31195bab4f494e86b90d859737922f3
Kimarialaing <42550463+Kimarialaing@users.noreply.github.com>
2018-11-27 12:04:07 -0500

    Add files via upload

---
+++ lambda javascript/isAdmin.js
@@ -0,0 +1,66 @@
+var mysql = require('./node_modules/mysql');
+var config = require('./config.json');
+//var validator = require('./validation.js');
+
+function formatErrorResponse(code, errs) {
+	return JSON.stringify({ 
+		error  : code,
+		errors : errs
+	});
+}
+
+exports.handler = (event, context, callback) => {
+
+	//validate input
+	var errors = new Array();
+	
+
+	
+	if(errors.length > 0) {
+		// This should be a "Bad Request" error
+		callback(formatErrorResponse('BAD_REQUEST', errors));
+	} else {
+	
+	//getConnection equivalent
+	var conn = mysql.createConnection({
+		host 	: config.dbhost,
+		user 	: config.dbuser,
+		password : config.dbpassword,
+		database : config.dbname
+	});
+	
+	//prevent timeout from waiting event loop
+	context.callbackWaitsForEmptyEventLoop = false;
+
+	//attempts to connect to the database
+	conn.connect(function(err) {
+	  	
+		if (err)  {
+			// This should be a "Internal Server Error" error
+			callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+		}
+		console.log("Connected!");
+		var sql = "SELECT isadmin FROM users WHERE userid = ?";
+		
+		conn.query(sql, [event.userid], function (err, result) {
+		  	if (err) {
+				// This should be a "Internal Server Error" error
+				callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+				} else if(!event.userid){
+					callback(formatErrorResponse("BAD_REQUEST", "User Does Not Exist"));
+				}
+				else {
+		  	 if(result[0].isadmin == 0){
+					callback(formatErrorResponse("BAD_REQUEST", "Access Denied"));
+		  	
+				}else{
+						console.log("Admin");
+		  		callback(null, "Admin");
+		  	
+				}}
+		  	}); //query registration codes
+		}); //connect database
+	} //no validation errors
+}; //handler
+
+
